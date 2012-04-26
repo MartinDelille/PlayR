5d886e9dcdd99dd66711805743647a66e5972b59
Martin Delille <martin.delille@gmail.com>
2012-04-26 15:27:00 +0200

    AMSerialPort support list enumeration

--- tests/DWSerialTest02/main.m
+++ tests/DWSerialTest02/main.m
@@ -10,30 +10,46 @@
 #import "DWTools/DWLogger.h"
 #import "AMSerialPort/AMSerialPort.h"
 #import "AMSerialPort/AMSerialPortAdditions.h"
+#import "AMSerialPort/AMSerialPortList.h"
 
 int main(int argc, const char * argv[])
 {
 
 	@autoreleasepool {
+		[DWLogger configureDisplay:NO showTime:YES showFile:NO showFunc:NO];
 		DWLog(@"SerialTest02");
-		NSString * deviceName = @"/dev/cu.usbserial-00001004A";
-		AMSerialPort * port = [[AMSerialPort alloc] init:deviceName withName:deviceName type:NULL];
-		
-		DWLog(@"Opening serial port");
-		if([port open]){
-			DWLog(@"CTS: %d", [port CTSOutputFlowControl]);
-			port.CTSOutputFlowControl = YES;
-			DWLog(@"CTS: %d", [port CTSOutputFlowControl]);
-			
-			DWLog(@"Reading data...");
-			NSString * s = [port readStringUsingEncoding:NSUTF8StringEncoding error:nil];
-//			[port writeString:@"pouet" usingEncoding:NSUTF8StringEncoding error:NULL];
-			DWLog(@"read : %@", s);
-			DWLog(@"Closing serial port");
-			[port close];
+		NSArray * portList = [[AMSerialPortList sharedPortList] serialPorts];
+		AMSerialPort * port = nil;
+		for (int i=0; i<portList.count; i++) {
+			NSString* name = [[portList objectAtIndex:i] name];
+			if ([name hasPrefix:@"usbserial"] && [name hasSuffix:@"A"]) {
+				port = [portList objectAtIndex:i];
+				break;
+			}
+		}
+
+		if (port == nil) {
+			DWLog(@"No usbserial port A available");
 		}
 		else {
-			DWLog(@"Unable to open serial port");
+			DWLog(@"Opening serial port");
+			if([port open]){
+				DWLog(@"CTS: %d", [port CTSOutputFlowControl]);
+				port.CTSOutputFlowControl = YES;
+				DWLog(@"CTS: %d", [port CTSOutputFlowControl]);
+				
+				//DWLog(@"Reading data...");
+				//NSString * s = [port readStringUsingEncoding:NSUTF8StringEncoding error:nil];
+				//DWLog(@"read : %@", s);
+				
+				DWLog(@"Writing data...");
+				[port writeString:@"pouet" usingEncoding:NSUTF8StringEncoding error:NULL];
+				DWLog(@"Closing serial port");
+				[port close];
+			}
+			else {
+				DWLog(@"Unable to open serial port");
+			}
 		}
 	    
 		DWLog(@"Bye bye");
