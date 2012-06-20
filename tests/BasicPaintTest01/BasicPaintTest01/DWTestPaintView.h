b0471b4b768aabc2481fa6bec8f84ea2cd1d4507
Martin Delille <martin.delille@gmail.com>
2012-06-20 14:52:12 +0200

    minimum size for DWPlayRVideo window

---
+++ tests/BasicPaintTest01/BasicPaintTest01/DWTestPaintView.h
@@ -0,0 +1,13 @@
+//
+//  DWTestPaintView.h
+//  BasicPaintTest01
+//
+//  Created by Martin Delille on 19/06/12.
+//  Copyright (c) 2012 Dubware. All rights reserved.
+//
+
+#import "DWPaintView.h"
+
+@interface DWTestPaintView : DWPaintView
+
+@end
---
+++ tests/BasicPaintTest01/BasicPaintTest01/DWTestPaintView.m
@@ -0,0 +1,28 @@
+//
+//  DWTestPaintView.m
+//  BasicPaintTest01
+//
+//  Created by Martin Delille on 19/06/12.
+//  Copyright (c) 2012 Dubware. All rights reserved.
+//
+
+#import "DWTestPaintView.h"
+
+@implementation DWTestPaintView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
